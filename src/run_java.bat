0e3955120cebd4d2916d865987215e26f7296c88
GraysS <antonpichka@gmail.com>
2021-07-28 02:04:06 +0300

    update file_2 run_java.bat

--- src/run_java.bat
+++ src/run_java.bat
@@ -1,1 +1,1 @@
-java --module-path "C:\pult-windows\jdk-16.0.2\javafx-sdk-16\lib" --add-modules=javafx.controls,javafx.fxml,javafx.graphics,javafx.base,javafx.media,javafx.swing,javafx.web Main
+java --module-path "C:\zov-pult-windows\jdk-16.0.2\javafx-sdk-16\lib" --add-modules=javafx.controls,javafx.fxml,javafx.graphics,javafx.base,javafx.media,javafx.swing,javafx.web Main
